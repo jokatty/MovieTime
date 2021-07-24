197ac7bcad863f6a528bed8b1cbc506063186272
jokatty <jyotikattani@gmail.com>
2021-07-24 22:02:17 +0800

    initial layout home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
+import MediaCard from './components/MediaCard.jsx';
 
 export default function App() {
   return (
     <div>
-      This is App.jsx
+      <Header />
+      <Hero />
+      <MediaCard />
     </div>
   );
 }
