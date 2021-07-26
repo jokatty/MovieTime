f49c0be7dc0e51cf044afc881e328afee66dc564
jokatty <jyotikattani@gmail.com>
2021-07-26 16:32:31 +0800

    test video in the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ export default function Hero() {
   return (
     <div className="hero">
       <p>hero container</p>
+      {/* <iframe width="100%" height="500" src="https://www.youtube.com/embed/YdAIBlPVe9s?start=21" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /> */}
     </div>
   );
 }
